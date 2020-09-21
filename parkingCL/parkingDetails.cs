c763380142086757d144ba655306d85d71b8e4d2
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-09-21 12:34:46 +0530

    Added:-model class

---
+++ parkingCL/parkingDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parkingCL
+{
+    class parkingDetails
+    {
+     
+       
+        public string VehicleOwnerName { get; set; }
+        public string DriverName { get; set; }
+        public string ParkingUserCategory { get; set; }
+        public string VehicleNumber { get; set; }
+        public string VehicalBrand { get; set; }       
+        public string VehicalColor { get; set; }      
+        public string ParkingSlot { get; set; }
+       
+        public string Status { get; set; }
+
+    }
+}
---
+++ parkingCL/unparkingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parkingCL
+{
+    class unparkingModel
+    {
+        public int VehicleUnParkID { get; set; }
+
+       
+        public int ParkingID { get; set; }
+
+        public string Status { get; set; }
+        public double TotalTime { get; set; }
+
+        public double TotalAmount { get; set; }
+    }
+}
