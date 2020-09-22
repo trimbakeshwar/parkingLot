525467902c18eedc5181d637e706f617925e7412
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-09-22 17:50:47 +0530

    Added:-implement swagger

--- parkingLot/Startup.cs
+++ parkingLot/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,10 +12,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using parkingBL.Interface;
 using parkingBL.services;
 using parkingRL.Interface;
 using parkingRL.services;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace parkingLot
 {
@@ -35,21 +40,53 @@ namespace parkingLot
             //a new instance is provided every time a service instance is requested
            services.AddTransient<RLInterface, parkingLotRL>();
             services.AddTransient<BLInterface, parkingLotBL>();
-            /*services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info
+            //Genetate token for user login
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
                 {
-                    Version = "v1",
-                    Title = "parking lot API",
-                    Description = "Asp.NET Core API",
-                    TermsOfService = "None",
-                    Contact = new Contact()
+                    var serverSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:key"]));
+                    options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        Name = "hudge trimbakeshwar",
-                        Email = "hudge.trimbakeshwar@gmail.com"
+                        IssuerSigningKey = serverSecret,
+                        ValidIssuer = Configuration["JWT:Issuer"],
+                        ValidAudience = Configuration["JWT:Audience"]
+                    };
+                });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "ParkingLot Api",
+                        Description = "Swagger Api",
+                        Version = "v1"
+                    });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
                     }
                 });
-            });*/
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,18 +100,16 @@ namespace parkingLot
             {
                 app.UseHsts();
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
+
             app.UseSwagger();
-            /*app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "parking lot API");
-            });*/
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "ParkingLot Portal");
+                options.RoutePrefix = "";
+            });
         }
     }
 }
