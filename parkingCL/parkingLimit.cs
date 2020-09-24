71094de5441c0cc32a415f07c944dfe229b0a15a
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-09-24 08:31:48 +0530

    Added:-parking slot model

---
+++ parkingCL/parkingLimit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parkingCL
+{
+    class parkingLimit
+    {
+        public double TotalParkingLimit = 16;
+        public double ParkingSlotA = 4;
+        public double ParkingSlotB = 4;
+        public double ParkingSlotC = 4;
+        public double ParkingSlotD = 4;
+    }
+}
